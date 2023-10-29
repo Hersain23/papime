6e687c5ffa1274304fab7fb20913721f29d0fdf8
FacebookConafe <conafe.facebook@gmail.com>
2023-10-28 22:54:45 -0600

    Se sube la primera version de responsive

---
+++ JS/footer.js
@@ -0,0 +1,15 @@
+/*let column1 = document.querySelector(".column1")
+let column2 = document.querySelector(".column2")
+let column3 = document.querySelector(".column3")
+
+var queryLarge = window.matchMedia("(max-width: 1220px)");
+queryLarge.addListener( function(EventoMediaQueryList) {
+      column1.classList.remove('col-5')
+      column1.classList.add('col-6')
+});
+
+var queryMedium = window.matchMedia("(max-width: 1030px)");
+queryLarge.addListener( function(EventoMediaQueryList) {
+      column1.classList.remove('col-6')
+      column1.classList.add('col-7')
+});*/
